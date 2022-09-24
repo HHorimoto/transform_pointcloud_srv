84f2688d8a22f3a7ef8c0b02f3fe5456cc352629
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2022-09-24 12:31:35 +0900

    add: source files

---
+++ src/transform_pointcloud_srv.cpp
@@ -0,0 +1,80 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <transform_pointcloud_srv/TransformPointcloud.h>
+#include <sensor_msgs/PointCloud2.h>
+
+class TfPointServer {
+    public:
+        TfPointServer();
+        ~TfPointServer() {};
+        bool transform(transform_pointcloud_srv::TransformPointcloud::Request &req, 
+                        transform_pointcloud_srv::TransformPointcloud::Response &res);
+        void loop();
+
+    private:
+        ros::NodeHandle nh;
+        ros::ServiceServer service;
+        tf::TransformListener tf_listener;
+};
+
+TfPointServer::TfPointServer()
+{
+    ROS_INFO("Ready to transform pointcloud");
+
+    service = nh.advertiseService("transform_pointcloud", &TfPointServer::transform, this);
+}
+
+bool TfPointServer::transform(transform_pointcloud_srv::TransformPointcloud::Request &req, 
+                        transform_pointcloud_srv::TransformPointcloud::Response &res)
+{
+    // http://docs.ros.org/en/melodic/api/roscpp/html/namespaceros_1_1topic.html
+    boost::shared_ptr<sensor_msgs::PointCloud2 const> pc_data = ros::topic::waitForMessage<sensor_msgs::PointCloud2>(req.topic_name.data, nh);
+    
+    if (pc_data) {
+        try
+        {
+            if (req.target_frame.data.empty() == false)
+            {
+                if (tf_listener.waitForTransform(req.target_frame.data, pc_data->header.frame_id, ros::Time(0), ros::Duration(4.0)) == false) {
+                    ROS_ERROR("Can't wait for transform");
+                    return false;
+                }
+                if (pcl_ros::transformPointCloud(req.target_frame.data, *pc_data, res.cloud_out, tf_listener) == false)
+                {
+                    ROS_ERROR("Failed pcl_ros::transformPointCloud target_frame:[%s]", req.target_frame.data.c_str());
+                    return false;
+                }
+                ROS_INFO("Processed successfully");
+                return true;
+            }
+            return false;
+        }
+        catch (std::exception &e)
+        {
+            ROS_ERROR("%s", e.what());
+            return false;
+        }
+    }
+    ROS_ERROR("Can't get message");
+    return false;
+}
+
+void TfPointServer::loop()
+{
+    ros::Rate loop_rate(30);                                                         
+    while(ros::ok()) {                                                           
+        ros::spinOnce();                                          
+        loop_rate.sleep();
+    }
+}
+
+int main(int argc, char **argv)
+{                                                    
+    ros::init(argc, argv, "transform_pointcloud_srv");
+
+    TfPointServer tfsvr;
+    tfsvr.loop();
+    return 0;
+}
---
+++ src/transform_pointcloud_srv/client_py.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+import std_msgs.msg
+from transform_pointcloud_srv.srv import TransformPointcloud
+
+def transform_pointcloud_srv(topic='/camera/depth/color/points', frame='/base_link'):
+    rospy.loginfo("TransformPointcloud Service")
+    frame_msg = std_msgs.msg.String()
+    frame_msg.data = frame
+    topic_name = std_msgs.msg.String()
+    topic_name.data = topic
+    rospy.wait_for_service('transform_pointcloud')
+    try:
+        client = rospy.ServiceProxy('transform_pointcloud', TransformPointcloud)
+        resp1 = client(topic_name, frame_msg)
+        # res : resp1.cloud_out
+        rospy.loginfo("Get transformed PointCloud2")
+        rospy.loginfo("info : frame_id = %s, height = %d, width = %d", 
+                      resp1.cloud_out.header.frame_id, resp1.cloud_out.height, resp1.cloud_out.width)
+    except rospy.ServiceException:
+        rospy.loginfo("Fail.")
+        
+def main():
+    rospy.init_node('client')
+    rospy.loginfo("Start TransformPointCloud")
+    rospy.sleep(1)
+    transform_pointcloud_srv()
+
+if __name__ == "__main__":
+    main()
