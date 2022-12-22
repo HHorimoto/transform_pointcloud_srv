9b8a0867678fb886734c2251a2bca80def4577a3
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2022-12-22 10:29:50 +0900

    update: test

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -4,27 +4,26 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import rospy
-import std_msgs.msg
 from transform_pointcloud_srv.srv import TransformPointcloud
 
-def transform_pointcloud_srv(topic='/velodyne_points', frame='/base_link'):
-    rospy.loginfo("TransformPointcloud Service")
+def transform_pointcloud_srv(topic, frame):
     rospy.wait_for_service('transform_pointcloud')
+    resp = None
     try:
         client = rospy.ServiceProxy('transform_pointcloud', TransformPointcloud)
-        resp1 = client(topic, frame)
-        # res : resp1.cloud_out
-        rospy.loginfo("Get transformed PointCloud2")
-        rospy.loginfo("info : frame_id = %s, height = %d, width = %d", 
-                      resp1.cloud_out.header.frame_id, resp1.cloud_out.height, resp1.cloud_out.width)
+        resp = client(topic, frame)
+        if resp:
+            rospy.loginfo("Run test succeeded")
     except rospy.ServiceException:
-        rospy.loginfo("Fail.")
+        rospy.loginfo("Run test failed")
         
 def main():
     rospy.init_node('client')
     rospy.loginfo("Start TransformPointCloud")
     rospy.sleep(1)
-    transform_pointcloud_srv()
+    topic_name = rospy.get_param('~topic_name', 'points')
+    target_frame = rospy.get_param('~target_frame', 'bar_link')
+    transform_pointcloud_srv(topic_name, target_frame)
 
 if __name__ == "__main__":
     main()
--- test/test.bash
+++ test/test.bash
@@ -3,23 +3,8 @@
 # SPDX-FileCopyrightText: 2022 Hiroto Horimoto
 # SPDX-License-Identifier: BSD-3-Clause
 
-### Confirm ros distro ###
-echo $ROS_DISTRO
+# Run Test #
+timeout 10 roslaunch transform_pointcloud_srv test.launch > test.log
 
-### Get rosbag ###
-wget "https://drive.google.com/uc?export=download&id=1MtkzAMVezawFd92f-6CYOH20NQbUgHW3" -O rosbag.bag
-
-### Run roscore in the background ###
-roscore &
-
-sleep 3
-
-### Run test script ###
-if [ $ROS_DISTRO = 'melodic' ]; then
-    python ./test/test_melodic.py
-elif [ $ROS_DISTRO = 'noetic' ]; then
-    python3 ./test/test_noetic.py
-else
-    echo "Other"
-    echo 1
-fi
+## Test ##
+grep "Run test succeeded" test.log
