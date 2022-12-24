62ba768c16b9e62799cb475b54cc1e6a18337d39
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2022-12-24 09:46:53 +0900

    change: launch py to launch xml

--- test/test.bash
+++ test/test.bash
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Run test #
-timeout 10 ros2 launch transform_pointcloud_srv test_launch.py > test.log
+timeout 10 ros2 launch transform_pointcloud_srv test.launch.xml > test.log
 
 ## Test ##
 grep "Run test succeeded" test.log
